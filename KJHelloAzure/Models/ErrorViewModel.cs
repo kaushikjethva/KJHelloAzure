888fc634ec1b8165dee6923686ec40357fbd85e6
Kaushik Jethva <Kaushik.Jethva@ey.com>
2020-04-27 21:30:47 +0530

    Add project files.

---
+++ KJHelloAzure/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace KJHelloAzure.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
